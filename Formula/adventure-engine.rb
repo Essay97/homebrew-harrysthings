059c62099d986130908206c8eb2d967d42e7104e
enrico <saggiorato.enrico@gmail.com>
2022-02-03 15:09:10 +0100

    Formula adventure-engine version 1.1.0

--- Formula/adventure-engine.rb
+++ Formula/adventure-engine.rb
@@ -3,13 +3,13 @@ require "formula"
 class AdventureEngine < Formula
     homepage "https://github.com/Essay97/tadvkt"
     desc "A CLI tool to create and play your own text adventure"
-    url "https://github.com/Essay97/tadvkt/releases/download/v1.0.3/adventure-engine-1.0.3.tar.gz"
-    sha256 "4615629a3c675485b7356877fa11c07afb95abae3e912346d334d211fa87d024"
-    version "1.0.3"
+    url "https://github.com/Essay97/tadvkt/releases/download/v1.1.0/adventure-engine-1.1.0.tar.gz"
+    sha256 "a68e03d3b78114ed392f267ad11666f441f9cf41da533cb121142127cb40f024"
+    version "1.1.0"
    
     def install
         inreplace "brew/adventure", "<PREFIX>", "#{prefix}"
-        prefix.install "adventure-engine-1.0.3-standalone.jar"
+        prefix.install "adventure-engine-1.1.0-standalone.jar"
         bin.install "brew/adventure"
     end
 end
