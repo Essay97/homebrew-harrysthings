42ca524974bc99bab916229e59fd6e4a4ce80ff7
enrico <saggiorato.enrico@gmail.com>
2023-04-24 01:11:39 +0200

    archie 1.0.0 (new formula)

---
+++ Formula/archie.rb
@@ -0,0 +1,12 @@
+class Archie < Formula 
+    desc "Create folder structures based on templates"
+    homepage "https://github.com/Essay97/archie"
+    url "https://github.com/Essay97/archie/archive/refs/tags/1.0.0.tar.gz"
+    sha256 "8b01d57d09d8b51de8648aa63585e5f2a21fec0f05a42b4aa3bfec0aa6260c7b"
+    license "MIT"
+    version "1.0.0"
+    depends_on "rust" => :build
+
+    def install
+        system "cargo", "build", "--release", "--bin", "archie"
+        bin.install "target/release/rustscan"
